899c05bd5d9d0988f97bb1ce2eb3aafa6170fc1d
ysiouda <ysiouda@erasme.org>
2019-04-18 14:37:41 +0200

    minor update on return state

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -16,11 +16,14 @@ class TableController extends Controller
         $item = Item::where('card_id', $state)->first();
         
         $media_url = array();
+        if(!$item){
+            return 'There is no state defined on the table';
+        }
         foreach($item->medias as $medias){
             array_push($media_url,asset('/medias').'/'.$medias );
         }
         $item->medias = $media_url;
-        //var_dump($item);
+        
         return $item;
     }
 
